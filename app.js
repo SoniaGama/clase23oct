92d2e605689a775feb83f7d300fe443df9eb2c26
SoniGama <abinyuuu@gmail.com>
2017-10-23 23:13:44 -0500

    prueba en windows

---
+++ app.js
@@ -0,0 +1,17 @@
+var pentaCoders = {
+  nombre1: "Edhit",
+  nombre2: "Tania",
+  caracteristicas: {altura: 1.55,
+                    edad: 29,
+                    colorPlayera: "negra"},
+  frutas:["manzana", "jicama", "piña"],
+  saludar: function(){
+    alert("hola equipo :D");
+  }
+}
+console.log(pentaCoders);
+pentaCoders.nombre3 = "Sonia";
+console.log(pentaCoders);
+pentaCoders.nombre1= "America";
+console.log(pentaCoders);
+//console.log(Object.keys(pentaCoders).length);
